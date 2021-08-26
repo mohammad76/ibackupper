a1f78513e0463c72c77052d509d8476bdf57e727
Mohammad <mohammad_abdi76@yahoo.com>
2021-08-26 13:46:20 +0430

    update

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='backupper',
+    name='ibackupper',
     version='0.3',
-    scripts=['backupper'],
+    scripts=['ibackupper'],
     author="Mohammad Abdi",
     author_email="mohammad_abdi76@yahoo.com",
     description="A backup helper",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/mohammad76/backupper",
+    url="https://github.com/mohammad76/ibackupper",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests', 'click', 'boto3'
